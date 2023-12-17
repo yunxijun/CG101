2a08e92c7466fd2eb6caedd17655ab4879ee02d5
Sunweijun333 <2318085206.qq.com>
2023-12-17 15:31:03 +0800

    添加项目文件。

---
+++ MFCBasic/MFCBasic/MFCBasicView.cpp
@@ -0,0 +1,105 @@
+﻿
+// MFCBasicView.cpp: CMFCBasicView 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFCBasic.h"
+#endif
+
+#include "MFCBasicDoc.h"
+#include "MFCBasicView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCBasicView
+
+IMPLEMENT_DYNCREATE(CMFCBasicView, CView)
+
+BEGIN_MESSAGE_MAP(CMFCBasicView, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CMFCBasicView 构造/析构
+
+CMFCBasicView::CMFCBasicView() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMFCBasicView::~CMFCBasicView()
+{
+}
+
+BOOL CMFCBasicView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFCBasicView 绘图
+
+void CMFCBasicView::OnDraw(CDC* /*pDC*/)
+{
+	CMFCBasicDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CMFCBasicView 打印
+
+BOOL CMFCBasicView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CMFCBasicView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CMFCBasicView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+
+// CMFCBasicView 诊断
+
+#ifdef _DEBUG
+void CMFCBasicView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFCBasicView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFCBasicDoc* CMFCBasicView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFCBasicDoc)));
+	return (CMFCBasicDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFCBasicView 消息处理程序
